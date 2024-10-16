ed848e07d47f45115fd796ebb2ea8b6ee01221d6
Ian Bruntlett <ian.bruntlett@gmail.com>
2024-10-16 18:38:42 +0100

    modified a key comment in hello-git.c

--- hello-git.c
+++ hello-git.c
@@ -1,4 +1,6 @@
 // hello-git.c, an experiment in using git to handle C programmes.
+// ... and  to see if backup files are respected via .gitignore file.
+// but emacs doesn't appear to make backup files any more.
 #include <stdio.h>
 
 int main(int argc, char *argv[])
