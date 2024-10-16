7cea09a7c4f7a9f9bb4761072415b411fc58f7b6
Ian Bruntlett <ian.bruntlett@gmail.com>
2024-10-16 17:52:24 +0100

    First attempt

---
+++ hello-git.c
@@ -0,0 +1,9 @@
+// hello-git.c, an experiment in using git to handle C programmes.
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Hello, World from %s\n",argv[0]);
+  return 0;
+}
+
