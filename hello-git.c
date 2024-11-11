c7d8f96f8b2eef4d48285a1edb71886c9af6f9b7
Ian Bruntlett <ian.bruntlett@gmail.com>
2024-11-11 11:29:23 +0000

    German option implemented

--- hello-git.c
+++ hello-git.c
@@ -8,6 +8,8 @@ int main(int argc, char *argv[])
 {
   if ( argc>=2 && 0==strcasecmp(argv[1],"French"))
     printf("Bonjour, Monde from %s\n",argv[0]);
+  else if ( argc>=2 && 0==strcasecmp(argv[1],"German"))
+    printf("Hallo, Welt from %s\n",argv[0]);
   else
     printf("Hello, World from %s\n",argv[0]);
   return 0;
