d989cb26ad9d241818ba99a3807952b7a7a33aee
Ian Bruntlett <ian.bruntlett@gmail.com>
2024-11-11 11:16:23 +0000

    reworked greeting to be English OR French

--- hello-git.c
+++ hello-git.c
@@ -2,10 +2,14 @@
 // ... and  to see if backup files are respected via .gitignore file.
 // but emacs doesn't appear to make backup files any more.
 #include <stdio.h>
+#include <string.h> // to get strcasecmp(). For Windows use stricmp()
 
 int main(int argc, char *argv[])
 {
-  printf("Bonjour, Monde from %s\n",argv[0]);
+  if ( argc>=2 && 0==strcasecmp(argv[1],"French"))
+    printf("Bonjour, Monde from %s\n",argv[0]);
+  else
+    printf("Hello, World from %s\n",argv[0]);
   return 0;
 }
 
