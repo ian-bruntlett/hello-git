2bc0a4eb62745c9267c91bf80bf6e19786df9ec9
Ian Bruntlett <ian.bruntlett@gmail.com>
2024-11-25 11:53:00 +0000

    Handle multiple command line arguments

--- hello-git.c
+++ hello-git.c
@@ -4,16 +4,24 @@
 #include <stdio.h>
 #include <string.h> // to get strcasecmp(). For Windows use stricmp()
 
+static void greeting(char *pProgramme, char *pLanguage)
+{
+  if ( 0==strcasecmp(pLanguage,"French"))
+    printf("Bonjour, Monde from %s\n",pProgramme);
+  else if ( 0==strcasecmp(pLanguage,"German"))
+    printf("Hallo, Welt from %s\n",pProgramme);
+  else if ( 0==strcasecmp(pLanguage,"Welsh"))
+    printf("Helo, Fyd from %s\n",pProgramme);
+  else
+    printf("Hello, World from %s\n",pProgramme);
+}
+
 int main(int argc, char *argv[])
 {
-  if ( argc>=2 && 0==strcasecmp(argv[1],"French"))
-    printf("Bonjour, Monde from %s\n",argv[0]);
-  else if ( argc>=2 && 0==strcasecmp(argv[1],"German"))
-    printf("Hallo, Welt from %s\n",argv[0]);
-  else if ( argc>=2 && 0==strcasecmp(argv[1],"Welsh"))
-    printf("Helo, Fyd from %s\n",argv[0]);
-  else
-    printf("Hello, World from %s\n",argv[0]);
+  
+  for  ( int index=1; index < argc; ++index )
+    {  greeting(argv[0],argv[index]); }
+   
   return 0;
 }
 
